5e2b89d1a01f779e8d51811b4f028555b3cf9146
proxygirl <hilahthomas@gmail.com>
2024-12-01 23:12:11 -0600

    Fixed image display
    Added image size adjustment in the block editor.
    Fixed image display inside posts.

--- schemas/fields/image.js
+++ schemas/fields/image.js
@@ -6,9 +6,24 @@ export default {
       type: 'string',
       title: 'Alternative text',
       description: 'Important for SEO and accessiblity.',
-        options: {
-          isHighlighted: true,
+      options: {
+        isHighlighted: true,
       },
     },
+    {
+      title: 'Image Size',
+      name: 'size',
+      type: 'string',
+      initialValue: '300',
+      description: 'The Small (300px) image size is best for posts.',
+      options: {
+        list: [
+          { title: 'Small (300px)', value: '300' },
+          { title: 'Medium (500px)', value: '500' },
+          { title: 'Large (700px)',  value: '700' },
+        ],
+      },
+      validation: rule => rule.required(),
+    },    
   ],
 }
